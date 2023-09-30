824cdaf88b6c783eea307660c440c4ff9e958ae5
Александр Савченко <savche@sfedu.ru>
2023-09-30 10:48:01 +0300

    Добавлена программа в которой реализован функционал проверки введённого года на високосность.

---
+++ time.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var y:= ReadInteger('Введите год: ');
+  if y mod 4 = 0 then
+    if (y mod 100 = 0) and (y mod 400 <> 0) then
+      print('Год не високосный')
+    else
+      print('Год високосный')
+  else
+    print('Год не високосный');
+end.
