3c24067bfbe280877fbbe4131d9fa061c6c8b318
Александр Савченко <savche@sfedu.ru>
2023-09-30 10:22:34 +0300

    Добавлена зависимость выводимого сообщения от времени

--- Hello.pas
+++ Hello.pas
@@ -1,2 +1,10 @@
-﻿##
-print('Hello, world!');
+﻿begin
+  var t := readinteger('Введите текущий час: ');
+  assert(t in 0..23);
+  case t of
+    4..10: print('Доброе утро, мир!');
+    11..16: print('Добрый день, мир!');
+    17..22: print('Добрый вечер, мир!');
+    else print('Доброй ночи, мир!');
+  end;
+end.
